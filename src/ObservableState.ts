c79b4fb3aec934509238b366da59b428c1cc08ac
Taylor Hutchison <seanthutchison@gmail.com>
2019-12-31 22:24:45 -0600

    Changed internal structure from BehaviorSubject.

--- src/ObservableState.ts
+++ src/ObservableState.ts
@@ -1,27 +1,38 @@
-import { BehaviorSubject, Subject, Subscription } from 'rxjs';
+import { Subject, Observable, Subscriber, Subscription } from 'rxjs';
 
-export abstract class ObservableState<T> {
+export abstract class ObservableState<T> extends Observable<T> {
 
-    // tslint:disable-next-line
-    private _state: BehaviorSubject<T>;
+    private _value!: Readonly<T>;
+    private _subscriber!: Subscriber<T>;
+    private _subscription: Subscription;
+    private _historyQueue: Queue<T>;
 
     constructor(initialState: T) {
-        this.deepFreeze(initialState);
-        this._state = new BehaviorSubject(initialState);
+        const obs = (subscriber: Subscriber<T>): void => {
+            this._subscriber = subscriber;
+        }
+        super(obs);
+        this._subscription = this.subscribe();
+        this._historyQueue = new Queue<T>(10);
+        ObservableState.deepFreeze(initialState);
+        this._value = initialState;
     }
-    
-    subscribe(next: null, error: null, complete: () => void): Subscription;
-    subscribe(next: null, error: (error: any) => void, complete?: () => void): Subscription;
-    subscribe(next: (value: Readonly<T>) => void, error: null, complete: () => void): Subscription;
-    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription;
+
+    subscribe(): Subscription;
+    subscribe(next?: (value: T) => void): Subscription
+    subscribe(next?: (value: T) => void, error?: (error: any) => void): Subscription
     subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription {
-        return this._state.subscribe(next, error, complete);
+        const subscription = super.subscribe(next, error, complete);
+        if(next && typeof next === 'function') {
+            next(this._value);
+        }
+        return subscription;
     }
 
     select(selector: (m: Readonly<T>) => any ) {
         const subject = new Subject();
-        let currentValue = selector(this._state.value);
-        this._state.subscribe((next) => {
+        let currentValue = selector(this._value);
+        this.subscribe((next) => {
             const newValue = selector(next);
             if (newValue !== currentValue) {
                 currentValue = newValue;
@@ -32,12 +43,17 @@ export abstract class ObservableState<T> {
     }
 
     protected update(func: (current: Readonly<T>) => T) {
-        const nextState = func(this._state.value);
-        this.deepFreeze(nextState);
-        this._state.next(nextState);
+        try {
+            const nextState = func(this._value);
+            ObservableState.deepFreeze(nextState);
+            this._historyQueue.enqueue(nextState);
+            this._subscriber.next(nextState);
+        } catch(err) {
+            this._subscriber.error(err);
+        }
     }
 
-    protected deepFreeze(obj: any) {
+    protected static deepFreeze(obj: any) {
         Object.freeze(obj);
         Object.getOwnPropertyNames(obj).forEach((prop) => {
           if (obj[prop] && ['object', 'function'].indexOf(typeof obj[prop]) !== -1) {
@@ -45,5 +61,21 @@ export abstract class ObservableState<T> {
           }
         });
         return obj;
+    }
+}
+
+const defaultCapacity: number = 10;
+class Queue<T> {
+    private _queue: T[];
+    private _capacity: number;
+    constructor(capacity: number) {
+        this._queue = [];
+        this._capacity = capacity > 0 ? capacity : defaultCapacity;
+    }
+    enqueue(item: T) {
+        if(this._queue.length >= this._capacity) {
+            this._queue.shift();
+        }
+        this._queue.push(item);
     }
 }
