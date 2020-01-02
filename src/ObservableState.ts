c490c84d1ba23aa7dab2ffb79a9bc72aa84f2245
Taylor Hutchison <seanthutchison@gmail.com>
2020-01-02 14:29:56 -0600

    Add undo functionality and tests.

--- src/ObservableState.ts
+++ src/ObservableState.ts
@@ -5,7 +5,7 @@ export abstract class ObservableState<T> extends Observable<T> {
     private _value!: Readonly<T>;
     private _subscriber!: Subscriber<T>;
     private _subscription: Subscription;
-    private _historyQueue: Queue<T>;
+    private _history: History<T>;
 
     constructor(initialState: T) {
         const obs = (subscriber: Subscriber<T>): void => {
@@ -13,8 +13,8 @@ export abstract class ObservableState<T> extends Observable<T> {
         }
         super(obs);
         this._subscription = this.subscribe();
-        this._historyQueue = new Queue<T>(10);
         ObservableState.deepFreeze(initialState);
+        this._history = new History<T>(10, initialState);
         this._value = initialState;
     }
 
@@ -23,13 +23,13 @@ export abstract class ObservableState<T> extends Observable<T> {
     subscribe(next?: (value: T) => void, error?: (error: any) => void): Subscription
     subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription {
         const subscription = super.subscribe(next, error, complete);
-        if(next && typeof next === 'function') {
+        if (next && typeof next === 'function') {
             next(this._value);
         }
         return subscription;
     }
 
-    select(selector: (m: Readonly<T>) => any ) {
+    select(selector: (m: Readonly<T>) => any) {
         const subject = new Subject();
         let currentValue = selector(this._value);
         this.subscribe((next) => {
@@ -46,36 +46,78 @@ export abstract class ObservableState<T> extends Observable<T> {
         try {
             const nextState = func(this._value);
             ObservableState.deepFreeze(nextState);
-            this._historyQueue.enqueue(nextState);
-            this._subscriber.next(nextState);
-        } catch(err) {
+            this._value = nextState;
+            this._history.enqueue(this._value);
+            this._subscriber.next(this._value);
+        } catch (err) {
             this._subscriber.error(err);
+        }
+    }
+
+    protected undo(): void {
+        const previousState = this._history.previous();
+        if(this._value != previousState) {
+            this._subscriber.next(previousState);
         }
     }
 
     protected static deepFreeze(obj: any) {
         Object.freeze(obj);
         Object.getOwnPropertyNames(obj).forEach((prop) => {
-          if (obj[prop] && ['object', 'function'].indexOf(typeof obj[prop]) !== -1) {
-            this.deepFreeze(obj[prop]);
-          }
+            if (obj[prop] && ['object', 'function'].indexOf(typeof obj[prop]) !== -1) {
+                this.deepFreeze(obj[prop]);
+            }
         });
         return obj;
     }
+
+
 }
 
 const defaultCapacity: number = 10;
-class Queue<T> {
-    private _queue: T[];
+export class History<T> {
+    public _queue: T[];
     private _capacity: number;
-    constructor(capacity: number) {
-        this._queue = [];
+    private _currentIndex: number;
+
+    constructor(capacity: number, initialState: T) {
+        this._queue = [initialState];
+        this._currentIndex = 0;
         this._capacity = capacity > 0 ? capacity : defaultCapacity;
     }
-    enqueue(item: T) {
-        if(this._queue.length >= this._capacity) {
-            this._queue.shift();
-        }
-        this._queue.push(item);
+
+    get oldest(): T {
+        return this._queue[0];
     }
+
+    get newest(): T {
+        return this._queue[this._queue.length - 1];
+    }
+
+    get current(): T {
+        return this._queue[this._currentIndex];
+    }
+
+    get count(): number {
+        return this._queue.length;
+    }
+
+    previous(): T {
+        if(this._currentIndex > 0) {
+            this._currentIndex = this._currentIndex - 1;
+        }
+        return this.current;
+    }
+
+    enqueue(item: T) {
+        if (this._queue.length >= this._capacity) {
+            this.dequeue();
+        }
+        this._currentIndex = this._queue.push(item) - 1;
+    }
+
+    dequeue() {
+        this._queue.shift();
+    }
+
 }
---
+++ tests/history.spec.ts
@@ -0,0 +1,66 @@
+import { ITestState } from './setup/ITestState';
+import { TestObservableState } from './setup/TestObservableState';
+import { initialState1 as initialState } from './setup/InitialState';
+import { History } from '../src/ObservableState';
+
+describe('Observable State History', () => {
+
+
+    let observableState: TestObservableState;
+    let testState: ITestState | null;
+
+    beforeEach(() => {
+        testState = null;
+        observableState = new TestObservableState(initialState);
+    })
+
+    describe('History class', () => {
+
+        it('should enqueue items to capacity then drop the oldest items', () => {
+            const history = new History<{num: number}>(10, {num: 0});
+            for (let i = 0; i <= 10; i++) {
+                history.enqueue({num: i});
+            }
+            console.log(history._queue);
+            expect(history.oldest.num).toBe(1);
+            expect(history.newest.num).toBe(10);
+        });
+        
+    });
+
+    it('should set the state to the previous state after an undo', () => {
+
+        const subscriber = observableState.subscribe((state) => {
+            testState = state;
+        });
+
+        expect(testState!.name).toBe(initialState.name);
+
+        observableState.update((state) => {
+            return {
+                ...state,
+                name: 'New Name'
+            }
+        });
+
+        expect(testState!.name).toBe('New Name');
+
+        observableState.undo();
+
+        expect(testState!.name).toBe(initialState.name);
+
+    });
+
+    it('should not go beyond the oldest state in the history queue', () => {
+
+        const subscriber = observableState.subscribe((state) => {
+            testState = state;
+        });
+
+        observableState.undo();
+        observableState.undo();
+
+        expect(testState!.name).toBe(initialState.name);
+
+    });
+});
